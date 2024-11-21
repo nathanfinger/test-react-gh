4df071accec9d9f5680c63bfbf2a072d4c3b1c72
felipeselau <felipeselau68@gmail.com>
2024-11-21 10:52:21 -0300

    added react router dom and started nps form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import { MultiStepForm } from "./components/MultiStepForm";
 
 
 export default function App() {
 
   return (
-      <MultiStepForm/>
+    <Routes>
+      <Route path="/" element={<MultiStepForm/>}/>
+      <Route path="/nps-form" element={<>WIP - NPS Form</>}/>
+    </Routes>
   )
 }
