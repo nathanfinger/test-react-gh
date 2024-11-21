22b0ceae054180bd3bc018934ac0bf1796c9e2c9
felipeselau <felipeselau68@gmail.com>
2024-11-21 00:55:26 -0300

    removed h1

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import { MultiStepForm } from "./components/MultiStepForm";
 export default function App() {
 
   return (
-    <>
-    <h1>Multi Step Form</h1>
       <MultiStepForm/>
-    </>
   )
 }
