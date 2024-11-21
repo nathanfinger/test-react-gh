65da9b3b9cba1e68ac68820f22a57d32f66b7871
felipeselau <felipeselau68@gmail.com>
2024-11-21 15:11:47 -0300

    changed browser router basename

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='react-gh'>
+    <BrowserRouter basename='/react-gh/'>
       <App />
     </BrowserRouter>
   </StrictMode>,
