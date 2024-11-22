06409791f75813f0e8736d076b6a57b9a66b7a47
felipeselau <felipeselau68@gmail.com>
2024-11-22 16:14:09 -0300

    added react router dom fallback

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-gh/'
+  base: '/react-gh/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
